5b3b4c4b7c4b0b0595c064efb281d27955b5e91f
governorx <xmanflips@gmail.com>
2019-10-14 00:16:06 -0400

    Created basic navbar as a example of component creation

--- bluemarket/src/App.jsx
+++ bluemarket/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Topbar from './components/Topbar.jsx';
 
 function App() {
   return (
     <div className="App">
+            <Topbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
