df0a8289c1efed0a6832e5dfb8899c882ee6eb8d
Kendall Douglass <kjdouglass52@gmail.com>
2019-10-28 13:14:54 -0400

    Changes to App.jsx, App.css, and added Homepage.jsx

--- bluemarket/src/App.jsx
+++ bluemarket/src/App.jsx
@@ -1,27 +1,21 @@
+//Import statements for each component.
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Topbar from './components/Topbar.jsx';
+import Homepage from './components/Homepage';
+
 
 function App() {
   return (
+    /*
+    * Topbar and Homepage are called in this manner to reduce LOC.
+    * Their code is located in the components folder.
+    */
     <div className="App">
-      <Topbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Topbar/>
+            <Homepage/>
     </div>
   );
 }
+
 export default App;
