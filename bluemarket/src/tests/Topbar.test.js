c47c5dabaf9d645d4000520ef21be5599eff5bd3
cornpop99 <taurenlb@gmail.com>
2019-10-29 14:57:41 -0400

    Added test for top bar and created new test folder.

---
+++ bluemarket/src/tests/Topbar.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import { Navbar, Nav, Form, FormControl, NavDropdown, Button } from 'react-bootstrap';
+import Topbar from '../components/Topbar.jsx';
+
+
+configure({adapter: new Adapter()});
+
+it('Should Render Topbar without crashing', () => {
+  const div = document.createElement('div');
+  render(<Topbar />, div);
+  unmountComponentAtNode(div);
+});
+
+it('Should link to #home', () => {
+  const wrapper = shallow(<Topbar />);
+  console.log(wrapper.find(Nav).at(0).props().children[2].props.children)
+  expect(wrapper.find(Nav).at(0).props().children[0].props.href).toBe('#home');
+  expect(wrapper.find(Nav).at(0).props().children[1].props.href).toBe('#link');
+  expect(wrapper.find(Nav).at(0).props().children[2].props.children).toBe('#link');
+});
