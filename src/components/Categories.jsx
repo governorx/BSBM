abd2e632bf880f4ac44032a227baf40bb7ac3741
Kendall Douglass <kdoug@Kendalls-MacBook-Air.local>
2019-11-18 17:17:44 -0500

    Changed images and added subcategories with text

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import clearance from '../images/clearance.png';
-import furniture from '../images/furniture.png';
+import clearance from '../images/clearance.jpg';
+import furniture from '../images/furniture.jpg';
 import academics from '../images/academics.jpg';
-import housing from '../images/housing.png';
-import jobsearch from '../images/jobsearch.png';
+import housing from '../images/housing.jpg';
+import jobsearch from '../images/jobsearch.jpg';
 import community from '../images/community.jpg';
+import Subcategories from './Subcategories';
 import '../App.css';
 export default class Categories extends Component {
 
@@ -13,23 +14,20 @@ export default class Categories extends Component {
 
         return (
 
-            <div className="row">
-                <div className="column">
-                    <img className="category-box" src={clearance} alt="clearance"/>
-                    <img className="category-box" src={community} alt="community"/>
+            <div class='Category-grid'>
+                <div class='Category-row'>
+                    <Subcategories src={jobsearch} alt="Job Search" />
+                    <Subcategories src={clearance} alt="Clearance" />
                 </div>
-                <div className="column">
-                    <img className="category-box" src={academics} alt="academics"/>
-                    <img className="category-box" src={jobsearch} alt="jobsearch"/>
+                <div class='Category-row'>
+                    <Subcategories src={housing} alt="Housing" />
+                    <Subcategories src={furniture} alt="Furniture" />
                 </div>
-                <div className="column">
-                    <img className="category-box" src={furniture} alt="furniture"/>
-                    <img className="category-box" src={housing} alt="housing"/>
+                <div class='Category-row'>
+                    <Subcategories src={academics} alt="Academics" />
+                    <Subcategories src={community} alt="Community" />
                 </div>
             </div>
-
-
         );
     }
-
 }
